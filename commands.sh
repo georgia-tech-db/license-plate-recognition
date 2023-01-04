c6689eba70b4445bbd6e0831d3934e43934b72b6
jarulraj <arulraj@gatech.edu>
2023-01-04 00:39:36 -0500

    checkpoint

--- commands.sh
+++ commands.sh
@@ -1,4 +1,8 @@
-python3 -m venv eva-application
-source eva-application/bin/activate
+python3 -m venv eva-application-venv
+source eva-application/bin/activate-venv
 
 pip install -r requirements.txt
+
+# Download model
+wget "https://www.dropbox.com/s/x677jwtae0elm6h/model.pth?dl=0"
+mv model.pth\?dl=0 model.pth
