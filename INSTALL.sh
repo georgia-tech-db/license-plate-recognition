ed4f47162e47fdcb0909260bf9494c9add2d18c9
jarulraj <arulraj@gatech.edu>
2023-01-05 00:38:43 -0500

    checkpoint

--- INSTALL.sh
+++ INSTALL.sh
@@ -11,16 +11,16 @@
 
 # Setup virtual environment
 python3 -m venv eva-application-venv
-source eva-application/bin/activate-venv
+source eva-application-venv/bin/activate
 
 # Install EVA application dependencies
 pip install -r requirements.txt
 
-# Download car plate detection model (based on image segmentation)
+# Download the car plate detection model (based on image segmentation)
 wget "https://www.dropbox.com/s/x677jwtae0elm6h/model.pth?dl=0"
 mv model.pth\?dl=0 model.pth
 
-# Refer the custome user-defined function (UDF)
+# Go over the custom user-defined function (UDF)
 cat car_plate_detector.py
 
 # Convert Jupyter notebook to README markdown
